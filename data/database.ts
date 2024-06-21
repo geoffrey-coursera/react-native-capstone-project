be62e18c80bc2caed0f9d847c887341d36472a11
geoffreyTools <geoffreyTools@users.noreply.github.com>
2024-06-21 13:29:23 +0200

    fix: return a fake database for the web renderer

--- data/database.ts
+++ data/database.ts
@@ -1,5 +1,22 @@
 export { db as default };
 
-import  {openDatabaseSync } from 'expo-sqlite';
+import  { SQLiteDatabase, openDatabaseSync } from 'expo-sqlite';
 
-const db = openDatabaseSync('little_lemon');
+type DB = Pick<SQLiteDatabase, 'runAsync' | 'getAllAsync'>;
+
+const db = instantiateDb('little_lemon');
+
+function instantiateDb (dbName: string) {
+    let db: DB;
+
+    try {
+        db = openDatabaseSync(dbName);
+    } catch (e) {
+        console.error('failed to open database connexion');
+        db = {
+            runAsync: () => Promise.reject(Error('no database')),
+            getAllAsync: () => Promise.reject(Error('no database'))
+        };
+    }
+    return db;
+}
